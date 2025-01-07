3fcefd6c0bb5f249339ef7acc095c0f61eaa68ac
Hanielle <saphielle.akiyama@gmail.com>
2025-01-07 16:17:44 +0900

    Simplify db handling

--- db.c
+++ db.c
@@ -1,17 +1,21 @@
 #include "db.h"
 
-int db_display() {
+int db_display(Database *db) {
     return 0;
 }
 
-int db_add(Movie movie) {
+int db_add(Database *db, Movie movie) {
     return 0;
 }
 
-int db_remove(int id) {
+int db_remove(Database *db, int id) {
     return 0;
 }
 
-int db_modify(int id) {
+int db_modify(Database *db, int id) {
+    return 0;
+}
+
+int db_commit(Database *db) {
     return 0;
 }
