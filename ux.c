5708b088cd8b8d814e8db0b9cdd19b0164f16164
Hanielle <saphielle.akiyama@gmail.com>
2025-01-11 15:50:14 +0900

    UX setup

---
+++ ux.c
@@ -0,0 +1,18 @@
+#include "ux.h"
+#include "db.h"
+
+void ux_display(Database *db) {
+
+}
+
+void ux_add(Database *db) {
+
+}
+
+void ux_delete(Database *db) {
+
+}
+
+void ux_edit(Database *db) {
+
+}
