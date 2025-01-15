e94255e66bdd44eeb4da0c500790fbabad5d7ea5
Hanielle <saphielle.akiyama@gmail.com>
2025-01-15 11:20:28 +0900

    Format

--- main.c
+++ main.c
@@ -3,145 +3,145 @@
 int line();
 
 void ux_display() {
-        FILE *file=fopen("movie.csv", "r");
-		int c;
-		while ((c=fgetc(file))!=EOF)
-			{
-			putchar(c);
-			}
-		fclose(file);
+    FILE *file=fopen("movie.csv", "r");
+    int c;
+    while ((c=fgetc(file))!=EOF)
+    {
+        putchar(c);
+    }
+    fclose(file);
 }
 
 void ux_add() {
-        FILE *file=fopen("movie.csv", "a");
-		
-		char title[50];
-		printf("Enter the title of the new movie\n");
-		scanf("%s", title);
+    FILE *file=fopen("movie.csv", "a");
 
-		char author[50];
-        printf("Enter the author of the new movie\n");
-        scanf("%s", author);
+    char title[50];
+    printf("Enter the title of the new movie\n");
+    scanf("%s", title);
 
-		char nationality[50];
-        printf("Enter the nationality of the new movie\n");
-        scanf("%s", nationality);
+    char author[50];
+    printf("Enter the author of the new movie\n");
+    scanf("%s", author);
 
-		char year[50];
-        printf("Enter the publication year of the new movie\n");
-        scanf("%s", year);
+    char nationality[50];
+    printf("Enter the nationality of the new movie\n");
+    scanf("%s", nationality);
 
-		char type[50];
-        printf("Enter the type of the new movie\n");
-        scanf("%s", type);
-		
-				
-		fprintf(file, "%d, %s, %s, %s, %s, %s",
-		line()+1, year, title, nationality, type, author);
+    char year[50];
+    printf("Enter the publication year of the new movie\n");
+    scanf("%s", year);
 
-		fclose(file);
+    char type[50];
+    printf("Enter the type of the new movie\n");
+    scanf("%s", type);
 
-                printf("Movie added successfully\n");
+
+    fprintf(file, "%d, %s, %s, %s, %s, %s",
+            line()+1, year, title, nationality, type, author);
+
+    fclose(file);
+
+    printf("Movie added successfully\n");
 }
 
 void ux_delete() {
-                                
-        int y, x, i, id_max;
-        printf("Enter the ID of the movie\n");
-        scanf("%d", &x);
-		FILE *rfile = fopen("movie.csv","r");
-        if (rfile == NULL)
-                {
-                printf("Error");
-                return 1;
-                }		
-		char movie[max_lines][max_line_length];
-        int id = 0;
-                
-        while (fgets(movie[id], max_line_length, rfile) != NULL)
-                {
-                id++;
-                }
-        fclose(rfile);
 
-		id_max = line();
+    int y, x, i, id_max;
+    printf("Enter the ID of the movie\n");
+    scanf("%d", &x);
+    FILE *rfile = fopen("movie.csv","r");
+    if (rfile == NULL)
+    {
+        printf("Error");
+        return 1;
+    }		
+    char movie[max_lines][max_line_length];
+    int id = 0;
 
-        FILE *wfile = fopen("movie.csv", "w");
+    while (fgets(movie[id], max_line_length, rfile) != NULL)
+    {
+        id++;
+    }
+    fclose(rfile);
 
-		while(i<x)
-			{
-            fputs(movie[i], wfile);
-			i++;
-			}
-		for(i=x+1;i< id_max; i++)
-            {
-			char *start, *end;
-            char data[100]="";
-			start = strchr(movie[i], ',');
-			end = strchr(start+1, '\n');
-			int number = end - start - 1;
-			strncpy(data, start+1, number);
-            fprintf(wfile, "%d,%s\n", i-1, data);
-            }
+    id_max = line();
 
-        fclose(wfile);
+    FILE *wfile = fopen("movie.csv", "w");
+
+    while(i<x)
+    {
+        fputs(movie[i], wfile);
+        i++;
+    }
+    for(i=x+1;i< id_max; i++)
+    {
+        char *start, *end;
+        char data[100]="";
+        start = strchr(movie[i], ',');
+        end = strchr(start+1, '\n');
+        int number = end - start - 1;
+        strncpy(data, start+1, number);
+        fprintf(wfile, "%d,%s\n", i-1, data);
+    }
+
+    fclose(wfile);
 }
 
 void ux_edit() {
-		int x;
-        printf("Enter the ID of the movie\n");
-        scanf("%d", &x);
+    int x;
+    printf("Enter the ID of the movie\n");
+    scanf("%d", &x);
 
-		char title[50];
-        printf("Enter the title of the new movie\n");
-        scanf("%s", title);
-                
-        char author[50];
-        printf("Enter the author of the new movie\n");
-        scanf("%s", author);
-                        
-        char nationality[50];
-        printf("Enter the nationality of the new movie\n");
-        scanf("%s", nationality);
-                
-        char year[50];
-        printf("Enter the publication year of the new movie\n");
-        scanf("%s", year);
-                                
-        char type[50];   
-        printf("Enter the type of the new movie\n");
-        scanf("%s", type);
-                
-        FILE *rfile = fopen("movie.csv","r");
-        if (rfile == NULL) 
-            {
-            printf("Error");
-            return 1;
-            }
-        char movie[max_lines][max_line_length];
-        int id = 0;
-                         
-        while (fgets(movie[id], max_line_length, rfile) != NULL)
-            {
-            id++;
-            }
-        fclose(rfile);
+    char title[50];
+    printf("Enter the title of the new movie\n");
+    scanf("%s", title);
 
-		snprintf(movie[x], max_line_length, "%d,%s,%s,%s,%s,%s\n", 
-			x, title, author, nationality, year, type);
+    char author[50];
+    printf("Enter the author of the new movie\n");
+    scanf("%s", author);
 
-		FILE *wfile = fopen("movie.csv", "w");
+    char nationality[50];
+    printf("Enter the nationality of the new movie\n");
+    scanf("%s", nationality);
 
-		for (int i = 0; i < id; i++) 
-			{
-            fputs(movie[i], wfile);
-        	}
-        	fclose(wfile);
+    char year[50];
+    printf("Enter the publication year of the new movie\n");
+    scanf("%s", year);
+
+    char type[50];   
+    printf("Enter the type of the new movie\n");
+    scanf("%s", type);
+
+    FILE *rfile = fopen("movie.csv","r");
+    if (rfile == NULL) 
+    {
+        printf("Error");
+        return 1;
+    }
+    char movie[max_lines][max_line_length];
+    int id = 0;
+
+    while (fgets(movie[id], max_line_length, rfile) != NULL)
+    {
+        id++;
+    }
+    fclose(rfile);
+
+    snprintf(movie[x], max_line_length, "%d,%s,%s,%s,%s,%s\n", 
+            x, title, author, nationality, year, type);
+
+    FILE *wfile = fopen("movie.csv", "w");
+
+    for (int i = 0; i < id; i++) 
+    {
+        fputs(movie[i], wfile);
+    }
+    fclose(wfile);
 
 }
 
 void ux_error() {
-       printf("You have to choose 1, 2, 3, 4 or 5"); 
+    printf("You have to choose 1, 2, 3, 4 or 5"); 
 }
 
 
